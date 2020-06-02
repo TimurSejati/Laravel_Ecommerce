8bd3132d9595bcb685c37f898f645eb390ffd2df
TimurSejati <timur7dian84@gmail.com>
2020-06-02 20:44:51 +0700

    Init project, kustomisasi template, multi autentifikasi user dan role

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.admin_layouts')
+
+@section('admin_content')
+  <div class="container">
+    <div style="height:100px"></div>
+    <center>
+      <h1>Admin Home Page</h1>
+    </center>
+  </div>
+@endsection
